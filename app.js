f371d4e977ff115d50317a1c70717a9a64b65ed9
Cesar <cesarpalma@localhost@cesarpalma.org>
2025-09-13 06:28:39 -0400

    Actualizar URL de la API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Asegúrate de cambiar la URL de la API a tu URL pública una vez que la despliegues.
 // Por ahora, usaremos la URL local para probar.
-const API_URL = 'http://127.0.0.1:5000/get_url'; 
+const API_URL = 'https://url-redirector-api.onrender.com/get_url';; 
 
 async function fetchAndRedirect() {
     try {
